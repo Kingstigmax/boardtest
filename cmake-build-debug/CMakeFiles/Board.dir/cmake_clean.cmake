670a71f6a5d251c8cbfe779266454be1c2424ea3
Kingstigmax <flofthouseprivate@gmail.com>
2024-06-19 17:54:32 +0200

    Test token

---
+++ cmake-build-debug/CMakeFiles/Board.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Board"
+  "Board.pdb"
+  "CMakeFiles/Board.dir/main.cpp.o"
+  "CMakeFiles/Board.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Board.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <cmath>
 #include <algorithm>
 
-
+// test
 enum class State {
     kEmpty,
     kObstacle,
